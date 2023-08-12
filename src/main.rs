581a37e7c6bdca71557dcb4f19d91a015f92130a
Jake Degiovanni <degiojake@gmail.com>
2023-08-12 17:40:01 +0100

    getting comic releases from marvel

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 use axum::{extract::State, http::StatusCode, response::Html, routing::get};
+use reqwest::Client;
+use serde_json::Value;
+use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::SystemTime;
 use tera::{Context, Tera};
+use tower_http::services::ServeDir;
 
 struct ComicShelf {
     tera: Tera,
+    client: Client,
 }
 
-async fn index(state: State<Arc<ComicShelf>>) -> Result<Html<String>, StatusCode> {
+async fn marvel_unlimited_comics(
+    State(state): State<Arc<ComicShelf>>,
+) -> Result<Html<String>, StatusCode> {
     let mut ctx = Context::new();
-    ctx.insert("name", "World");
+    ctx.insert("PageEndpoint", "/marvel-unlimited/comics");
+    ctx.insert("Date", "2023-08-12");
 
-    let body = match state.tera.render("index1.html", &ctx) {
+    let ts = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+    let hash = "";
+
+    let result = match state.client.get(format!("https://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateRange=2023-01-01,2023-01-07&hasDigitalIssue=true&orderBy=issueNumber&limit=100&apikey=...&ts={}&hash={}", ts, hash)).send().await {
+        Ok(r) => r,
+        Err(e) => {
+            println!("http client error: {}", e);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
+    .text()
+    .await;
+
+    let result: HashMap<String, Value> = match result {
+        Ok(r) => serde_json::from_str(&r).unwrap(),
+        Err(e) => {
+            println!("text errors: {}", e);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    ctx.insert("results", &result);
+
+    let body = match state.tera.render("marvel-unlimited.html", &ctx) {
         Ok(b) => b,
         Err(e) => {
             println!("tera rendering error: {}", e);
@@ -31,8 +66,13 @@ async fn main() {
         }
     };
 
-    let state = Arc::new(ComicShelf { tera });
-    let app = axum::Router::new().route("/", get(index)).with_state(state);
+    let client = Client::new();
+    let state = Arc::new(ComicShelf { tera, client });
+
+    let app = axum::Router::new()
+        .route("/marvel-unlimited/comics", get(marvel_unlimited_comics))
+        .nest_service("/static", ServeDir::new("static"))
+        .with_state(state);
 
     axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
         .serve(app.into_make_service())
