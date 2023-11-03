b72f94c9557dc691bf86476eca931b8caec23cbf
Jake Degiovanni <degiojake@gmail.com>
2023-11-03 21:53:53 +0000

    improved the error handling in the etag middleware + some todo notes

--- src/marvel/etag.rs
+++ src/marvel/etag.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 use std::task::{Context, Poll};
 
+use anyhow::anyhow;
 use futures_util::future::BoxFuture;
 use hyper::{Body, Request, Response, StatusCode};
 use tower::{BoxError, Layer, Service};
@@ -44,13 +45,14 @@ impl<S> EtagCacheMiddleware<S> {
     }
 }
 
+// todo really want this to follow the S::Error: Into<BoxError> with no Error constraint on the Service trait itself
 impl<S> Service<Request<Body>> for EtagCacheMiddleware<S>
 where
     S: Service<Request<Body>, Response = Response<Body>, Error = BoxError> + Clone + Send + 'static,
     S::Future: Send,
 {
     type Response = DataWrapper;
-    type Error = BoxError;
+    type Error = BoxError; // todo can a more specific error be returned here?
     type Future = BoxFuture<'static, Result<DataWrapper, Self::Error>>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
@@ -61,36 +63,44 @@ where
         let this = self.inner.clone();
         let mut this = std::mem::replace(&mut self.inner, this);
 
-        let key = req.uri().path_and_query().unwrap().clone();
-        let mut headers = req.headers().clone();
-
-        let (mut p, b) = req.into_parts();
-
-        match self.cache.read().unwrap().get(key.as_str()) {
-            Some(wrapper) => {
-                println!(
-                    "key {:?} exists in cache, using etag {:?}",
-                    key, wrapper.etag
-                );
-
-                headers.insert("If-None-Match", wrapper.etag.parse().unwrap());
-            }
-            None => {
-                println!("key {:?} does not exist in cache", key);
-            }
-        }
-
-        p.headers = headers;
-        let req = Request::from_parts(p, b);
-
+        let req = req;
         let cache = self.cache.clone();
-        let key = key.to_string();
 
         Box::pin(async move {
+            let key = req.uri().path_and_query()
+                .ok_or(anyhow!("no path or query"))?
+                .clone();
+            let mut headers = req.headers().clone();
+
+            let (mut p, b) = req.into_parts();
+
+            // todo how to properly handle poisoned locks?
+            match cache.read().expect("could not read from the cache").get(key.as_str()) {
+                Some(wrapper) => {
+                    println!(
+                        "key {:?} exists in cache, using etag {:?}",
+                        key, wrapper.etag
+                    );
+
+                    headers.insert("If-None-Match", wrapper.etag.parse()?);
+                }
+                None => {
+                    println!("key {:?} does not exist in cache", key);
+                }
+            }
+
+            p.headers = headers;
+            let req = Request::from_parts(p, b);
+
+            let key = key.to_string();
+
             let response = this.call(req).await?;
             if response.status() == StatusCode::NOT_MODIFIED {
                 println!("using cache");
-                Ok(cache.read().unwrap().get(key.as_str()).unwrap().clone())
+                Ok(cache.read().expect("could not read from the cache")
+                    .get(key.as_str())
+                    .ok_or(anyhow!("an item expected to be in the cache could not be found"))?
+                    .clone())
             } else {
                 let result: DataWrapper = serde_json::from_slice(
                     hyper::body::to_bytes(response.into_body())
@@ -99,7 +109,7 @@ where
                         .as_slice(),
                 )?;
                 println!("storing cache");
-                cache.write().unwrap().insert(key, result.clone());
+                cache.write().expect("could not write to the cache").insert(key, result.clone());
                 Ok(result)
             }
         })
