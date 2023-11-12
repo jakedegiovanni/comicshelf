a777fa014cd7bf326ff15d9d31741f40c2baf052
Jake Degiovanni <degiojake@gmail.com>
2023-11-12 18:09:23 +0000

    added tracing

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,9 @@ use marvel::router::MARVEL_PATH;
 use serde_json::Value;
 use tera::Tera;
 use thiserror::Error;
-use tower::BoxError;
+use tower::{BoxError, ServiceBuilder};
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
 
 mod marvel;
 mod middleware;
@@ -62,9 +63,14 @@ async fn main() {
     let https = HttpsConnector::new();
     let client = hyper::Client::builder().build::<_, hyper::Body>(https);
 
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let app = axum::Router::new()
         .nest(MARVEL_PATH, marvel::router::new(tera, &client))
-        .nest_service("/static", ServeDir::new("static"));
+        .nest_service("/static", ServeDir::new("static"))
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
         .serve(app.into_make_service())
