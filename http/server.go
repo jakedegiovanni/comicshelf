f7083e107af6d358993ee6ddd6b891c0c7ea7f01
Jake Degiovanni <degiojake@gmail.com>
2024-01-29 14:43:38 +0000

    start of an http package

---
+++ http/server.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type ServerConfig struct {
+	Address string
+}
+
+func NewServer(config ServerConfig) (*http.Server, *chi.Mux) {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Recoverer)
+
+	return &http.Server{
+		Handler: router,
+		Addr:    config.Address,
+	}, router
+}
