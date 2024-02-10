063fe5dbfdd2292d5410cd55a1c9328547bc345d
Jake Degiovanni <degiojake@gmail.com>
2024-02-10 17:01:07 +0000

    migrated marvel sdk as is to new file structure

--- comicclient/marvel/config.go
+++ comicclient/marvel/config.go
@@ -5,5 +5,7 @@ import (
 )
 
 type Config struct {
-	Client comicclient.Config `mapstructure:"client"`
+	Client        comicclient.Config `mapstructure:"client"`
+	DateLayout    string             `mapstructure:"date_layout"`
+	ReleaseOffset int                `mapstructure:"release_offset"`
 }
--- comicclient/marvel/marvel.go
+++ comicclient/marvel/marvel.go
@@ -2,10 +2,10 @@ package marvel
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/jakedegiovanni/comicshelf/comicclient"
@@ -80,6 +80,7 @@ type Client struct {
 	comicCache  *Cache[*DataWrapper[Comic]]
 	seriesCache *Cache[*DataWrapper[Series]]
 	logger      *slog.Logger
+	cfg         *Config
 }
 
 func New(cfg *Config, logger *slog.Logger) *Client {
@@ -89,19 +90,65 @@ func New(cfg *Config, logger *slog.Logger) *Client {
 			apiKeyMiddleware(logger),
 		)),
 		logger:      logger,
+		cfg:         cfg,
 		comicCache:  NewCache[*DataWrapper[Comic]](),
 		seriesCache: NewCache[*DataWrapper[Series]](),
 	}
 }
 
 func (c *Client) GetWeeklyComics(t time.Time) (*DataWrapper[Comic], error) {
-	return nil, errors.New("implement me")
+	if t.Weekday() == time.Sunday {
+		// sunday is when the cut off is however new comics show up on the monday, the offset set here makes expectations match reality
+		t = t.AddDate(0, 0, -1)
+	}
+
+	first, last := c.weekRange(c.marvelUnlimitedDate(t))
+	endpoint := fmt.Sprintf("/comics?format=comic&formatType=comic&noVariants=true&dateRange=%s,%s&hasDigitalIssue=true&orderBy=issueNumber&limit=100", first.Format(c.cfg.DateLayout), last.Format(c.cfg.DateLayout))
+
+	return request[Comic](endpoint, c.comicCache, c.client, c.logger)
 }
 
 func (c *Client) GetComic(endpoint string) (*DataWrapper[Comic], error) {
+	return request[Comic](endpoint, c.comicCache, c.client, c.logger)
+}
+
+func (c *Client) GetComicsWithinSeries(seriesEndpoint string) (*DataWrapper[Comic], error) {
+	uri, err := url.Parse(seriesEndpoint)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse endpoint for series comics retrieval: %w", err)
+	}
+
+	uri.Path = fmt.Sprintf("%s/comics", uri.Path)
+	query := uri.Query()
+	query.Add("format", "comic")
+	query.Add("formatType", "comic")
+	query.Add("noVariants", "true")
+	query.Add("hasDigitalIssue", "true")
+	query.Add("orderBy", "issueNumber")
+	query.Add("limit", "100")
+	uri.RawQuery = query.Encode()
+
+	return request[Comic](uri.String(), c.comicCache, c.client, c.logger)
+}
+
+func (c *Client) weekRange(t time.Time) (time.Time, time.Time) {
+	for t.Weekday() != time.Sunday {
+		t = t.AddDate(0, 0, -1)
+	}
+
+	t = t.AddDate(0, 0, -7)
+
+	return t, t.AddDate(0, 0, 6)
+}
+
+func (c *Client) marvelUnlimitedDate(t time.Time) time.Time {
+	return t.AddDate(0, c.cfg.ReleaseOffset, 0)
+}
+
+func request[T any](endpoint string, cache *Cache[*DataWrapper[T]], client *http.Client, logger *slog.Logger) (*DataWrapper[T], error) {
 	var resp *http.Response
 
-	if data, ok := c.comicCache.Get(endpoint); ok {
+	if data, ok := cache.Get(endpoint); ok {
 		req, err := http.NewRequest(http.MethodGet, endpoint, nil)
 		if err != nil {
 			return nil, fmt.Errorf("could not create request: %w", err)
@@ -109,20 +156,20 @@ func (c *Client) GetComic(endpoint string) (*DataWrapper[Comic], error) {
 
 		req.Header.Set("If-None-Match", data.Etag)
 
-		resp, err = c.client.Do(req)
+		resp, err = client.Do(req)
 		if err != nil {
 			return nil, fmt.Errorf("error whilst performing request: %w", err)
 		}
 
 		if resp.StatusCode == http.StatusNotModified {
-			c.logger.Debug("not modified, using cached response")
+			logger.Debug("not modified, using cached response")
 			return data, nil
 		}
 	} else {
-		c.logger.Debug("item not present in cache")
+		logger.Debug("item not present in cache")
 
 		var err error
-		resp, err = c.client.Get(endpoint)
+		resp, err = client.Get(endpoint)
 		if err != nil {
 			return nil, fmt.Errorf("error whilst performing request: %w", err)
 		}
@@ -130,16 +177,12 @@ func (c *Client) GetComic(endpoint string) (*DataWrapper[Comic], error) {
 
 	defer resp.Body.Close()
 
-	var d DataWrapper[Comic]
+	var d DataWrapper[T]
 	err := json.NewDecoder(resp.Body).Decode(&d)
 	if err != nil {
 		return nil, fmt.Errorf("could not decode data wrapper: %w", err)
 	}
 
-	c.comicCache.Put(endpoint, &d)
+	cache.Put(endpoint, &d)
 	return &d, nil
-}
-
-func (c *Client) GetComicsWithinSeries(seriesEndpoint string) (*DataWrapper[Comic], error) {
-	return nil, errors.New("implement me")
 }
