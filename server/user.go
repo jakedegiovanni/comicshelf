da49d6964ac7fb8ed646e09c4f7e55a244f21bde
Jake Degiovanni <degiojake@gmail.com>
2024-02-17 14:13:43 +0000

    implement user api

--- server/user.go
+++ server/user.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"errors"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -12,23 +16,58 @@ func (s *Server) registerUserRoutes(router chi.Router) {
 }
 
 func (s *Server) registerFollow(w http.ResponseWriter, r *http.Request) {
-	_ = r.ParseForm()
-	_ = r.PostFormValue("key")
-	_ = r.PostFormValue("name")
-	// if a.db.Following(key) {
-	// 	a.db.Unfollow(key)
-	// 	err := a.tmpl.ExecuteTemplate(w, "follow", nil)
-	// 	if err != nil {
-	// 		a.logger.Warn("error writing unfollow", slog.String("err", err.Error()))
-	// 	}
-	// 	return
-	// }
+	id, err := s.extractSeriesIdFromForm(r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not extract series id: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
 
-	// a.db.Follow(key, name)
-	// err := a.tmpl.ExecuteTemplate(w, "unfollow", nil)
-	// if err != nil {
-	// 	a.logger.Warn("error writing follow", slog.String("err", err.Error()))
-	// }
+	err = s.user.Follow(r.Context(), 0, id) // using default user id until auth actually implemented
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not follow series with id: %d", id), http.StatusInternalServerError)
+		return
+	}
+
+	err = s.tmpl.ExecuteTemplate(w, "unfollow", nil)
+	if err != nil {
+		slog.Warn("error writing follow", slog.String("err", err.Error()))
+	}
 }
 
-func (s *Server) registerUnfollow(w http.ResponseWriter, r *http.Request) {}
+func (s *Server) registerUnfollow(w http.ResponseWriter, r *http.Request) {
+	id, err := s.extractSeriesIdFromForm(r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not extract series id: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	err = s.user.Unfollow(r.Context(), 0, id) // using default user id until auth actually implemented
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not unfollow series with id: %d", id), http.StatusInternalServerError)
+		return
+	}
+
+	err = s.tmpl.ExecuteTemplate(w, "follow", nil)
+	if err != nil {
+		slog.Warn("error writing follow", slog.String("err", err.Error()))
+	}
+}
+
+func (s *Server) extractSeriesIdFromForm(r *http.Request) (int, error) {
+	err := r.ParseForm()
+	if err != nil {
+		return 0, errors.New("could not read form")
+	}
+
+	seriesId := r.PostFormValue("series")
+	if seriesId == "" {
+		return 0, errors.New("series key not present")
+	}
+
+	id, err := strconv.Atoi(seriesId)
+	if err != nil {
+		return 0, fmt.Errorf("series id is not a valid number: %s", seriesId)
+	}
+
+	return id, nil
+}
