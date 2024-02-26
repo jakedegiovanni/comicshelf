bcb8d7025ac512e77b44ec6b673ca55e4e0747c8
Jake Degiovanni <degiojake@gmail.com>
2024-02-26 13:00:04 +0000

    removing duplicates

--- comicclient/comicclient.go
+++ comicclient/comicclient.go
@@ -6,22 +6,7 @@ import (
 
 func New(cfg *Config, middleware ...Middleware) *http.Client {
 	return &http.Client{
-		Transport: ClientMiddlewareChain(middleware...)(http.DefaultTransport),
+		Transport: MiddlewareChain(middleware...)(http.DefaultTransport),
 		Timeout:   cfg.Timeout,
-	}
-}
-
-func ClientMiddlewareChain(chain ...Middleware) Middleware {
-	return func(next http.RoundTripper) http.RoundTripper {
-		if len(chain) == 0 {
-			return next
-		}
-
-		wrapped := next
-		for i := len(chain) - 1; i >= 0; i-- {
-			wrapped = chain[i](wrapped)
-		}
-
-		return wrapped
 	}
 }
